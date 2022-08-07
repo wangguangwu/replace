5cd6e471d52fa81bd964f1bee32fa1b4dd8eadfb
wangguangwu <wangguangwu@pukanghealth.com>
2022-08-07 23:51:20 +0800

    replace-aqs

---
+++ replace-aqs/src/main/java/com/wangguangwu/test/TestR.java
@@ -0,0 +1,33 @@
+package com.wangguangwu.test;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+//import util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author wangguangwu
+ */
+public class TestR {
+
+    private static final ReentrantLock LOCK = new ReentrantLock();
+
+    public static void main(String[] args) {
+        int times = 10;
+        for (int i = 0; i < times; i++) {
+            Thread thread = new Thread(() -> {
+                LOCK.lock();
+                try {
+                    Thread.sleep(1000);
+                    System.out.println("Hello World");
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    LOCK.unlock();
+                }
+            });
+            thread.start();
+        }
+        System.out.println("Hello wangguangwu");
+    }
+
+}
---
+++ replace-aqs/src/main/java/util/concurrent/locks/ReentrantLock.java
@@ -0,0 +1,16 @@
+package util.concurrent.locks;
+
+/**
+ * @author wangguangwu
+ */
+public class ReentrantLock {
+
+    public static void lock() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void unlock() {
+        throw new UnsupportedOperationException();
+    }
+
+}
